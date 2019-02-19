b108840327c32cd5e59c6dea9aa286de86a02d25
hslee1539 <qq1539@naver.com>
2019-02-19 15:07:55 +0900

    사용성 개선 및, 오류 수정

--- layer/batchnormalization_module.py
+++ layer/batchnormalization_module.py
@@ -3,24 +3,25 @@ from . import interface_module
 import tensor
 
 class Batchnormalization(interface_module.Forwardable, interface_module.Backwardable):
-    def __init__(self, dispersion):
+    """정규화 층입니다."""
+    def __init__(self):
         self.out = tensor.Tensor([0], [0,1])
-        self.dispersion = dispersion
+        self.dispersion = None
 
     def forward(self, x):
         if(self.out.shape[0] != x.shape[0]):
             self.out = x.copy()
-        
-        return self.forward_line(self, x)
+            self.dispersion = tensor.create_zeros([x.shape[0]])
+        return self.forward_line(x)
 
     def forward_line(self, x):
         computing.forward(x.array, x.shape, self.dispersion.array, self.out.array)
         return self.out
 
     def backward(self, dx):
-        return self.backward_line(self, dx)
+        return self.backward_line(dx)
 
     def backward_line(self, dx):
-        computing.forward(dx.array, self.dispersion.array, self.out.array)
+        computing.backward(dx.array, self.dispersion.array, self.out.array)
         return self.out
     
