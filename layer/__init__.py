1d46080dba1b6a5a0e6ff254ee02bfce58cafa31
hslee1539 <qq1539@naver.com>
2019-02-23 15:09:12 +0900

    Conv3d와 CrossEntroy가 import 되게 수정

--- layer/__init__.py
+++ layer/__init__.py
@@ -8,3 +8,5 @@ from .sigmoid_module import Sigmoid
 from .softmax_module import Softmax
 from .shift_module import Shift
 from .batchnormalization_module import Batchnormalization
+from .conv3d_module import Conv3d
+from .cross_entropy_module import CrossEntropy
